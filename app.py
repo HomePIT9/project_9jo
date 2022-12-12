b6a67525f505c79428d9b8cb470585e9074c231f
ggobsarikuna <hyunwu123@naver.com>
2022-12-12 20:27:26 +0900

    비밀번호 안보이게, 비밀번호 재확인

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import hashlib
 import jwt
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 
+
+
 app = Flask(__name__)
+
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
 
 from pymongo import MongoClient
 import certifi
@@ -95,6 +101,8 @@ def id_check():
     id_list = list(db.save_info.find({}, {'_id': False}))
 
     return jsonify({'id_lists': id_list})
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
